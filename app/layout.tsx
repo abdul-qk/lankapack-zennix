e8ca62745be38bc4b914801e59679930c105a352
abdul-qk <abdulsmart100@gmail.com>
2025-04-30 14:19:14 +0530

    Added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "/fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Toaster />
         <Analytics />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
