9db0825a0e308bde1907117dc931eb8f4fffee45
abdul-qk <abdulsmart100@gmail.com>
2025-05-07 23:11:15 +0530

    Update prisma transaction timeout:

--- app/api/material/material-receiving-note/edit/[id]/route.ts
+++ app/api/material/material-receiving-note/edit/[id]/route.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  transactionOptions: {
+    maxWait: 5000, // default: 2000
+    timeout: 10000, // default: 5000
+  },
+});
 
 export async function GET(
   req: Request,
