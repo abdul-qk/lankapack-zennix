f305231ddd3a7b6734e77c0983271bb50debbb32
abdul-qk <abdulsmart100@gmail.com>
2025-05-15 12:43:36 +0530

    Update

--- app/api/sales/do-numbers/route.ts
+++ app/api/sales/do-numbers/route.ts
@@ -1,6 +1,6 @@
+export const dynamic = "force-dynamic";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-export const dynamic = "force-dynamic";
 
 const prisma = new PrismaClient();
 
