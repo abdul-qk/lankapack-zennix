70fbb6e5ef530dd7a9564693c54552557beaacd2
abdul-qk <abdulsmart100@gmail.com>
2025-08-10 22:23:20 +0530

    feat(material-items): add GET endpoint to fetch material items with particulars

---
+++ app/api/material/material-items/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req: Request) {
+  try {
+    // Fetch all filtered material info data with valid suppliers
+    const materialItems = await prisma.hps_material_item.findMany({
+        include: {
+            particular: {
+                select: {
+                    particular_name: true,
+                }
+            },
+        },
+        distinct: ['material_item_gsm'],
+
+    });
+    console.log(materialItems);
+
+
+    return new Response(JSON.stringify({ data: materialItems }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error fetching material items:", error);
+    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+      status: 500,
+    });
+  }
+}
